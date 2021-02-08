cc5ef854845352668db07c81574b0e3a049c87ec
kai <kai@turbot.com>
2021-02-08 13:23:05 +0000

    list call and hydrate call counting

--- chaos/table_chaos_multi_region.go
+++ chaos/table_chaos_multi_region.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/plugin"
@@ -11,6 +12,10 @@ import (
 )
 
 const fetchMetdataKeyRegion = "region"
+
+var listCalls int
+var hydrateCalls int
+var mut sync.Mutex
 
 func GetRegions(_ context.Context, config interface{}) []map[string]interface{} {
 	chaosConfig := config.(chaosConfig)
@@ -39,24 +44,44 @@ func multiRegionTable() *plugin.Table {
 		Columns: []*plugin.Column{
 			{Name: "id", Type: proto.ColumnType_STRING},
 			{Name: "region", Type: proto.ColumnType_STRING, Transform: transform.FromFetchMetadata(fetchMetdataKeyRegion)},
+			{Name: "list_calls", Type: proto.ColumnType_INT, Hydrate: getListCalls, Transform: transform.FromValue()},
+			{Name: "hydrate_calls", Type: proto.ColumnType_INT, Hydrate: getHydrateCalls, Transform: transform.FromValue()},
+			{Name: "c1", Type: proto.ColumnType_STRING, Hydrate: doHydrate1},
+			{Name: "c2", Type: proto.ColumnType_STRING, Hydrate: doHydrate2},
+			{Name: "c3", Type: proto.ColumnType_STRING, Hydrate: doHydrate3},
+			{Name: "c4", Type: proto.ColumnType_STRING, Hydrate: doHydrate4},
+			{Name: "c5", Type: proto.ColumnType_STRING, Hydrate: doHydrate5},
+		},
+		HydrateConfig: []plugin.HydrateConfig{
+			{
+				Func:    getHydrateCalls,
+				Depends: []plugin.HydrateFunc{doHydrate1, doHydrate2, doHydrate3, doHydrate4, doHydrate5},
+			},
 		},
 	}
 }
 
 func regionAwareList(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	mut.Lock()
+	defer mut.Unlock()
+
 	region := regionFromFetchMetadata(ctx)
+	plugin.Logger(ctx).Warn("regionAwareList", "region", region)
 	if region == "" {
 		return nil, nil
 	}
 
 	plugin.Logger(ctx).Warn("regionAwareList", "region", region)
 
-	for i := 0; i < 5; i++ {
+	const itemsPerRegion = 5
+	for i := 0; i < itemsPerRegion; i++ {
 		id := buildId(i, region)
 		item := map[string]interface{}{"id": id}
 		plugin.Logger(ctx).Warn("regionAwareList", "item", item)
 		d.StreamListItem(ctx, item)
 	}
+	// update counter
+	listCalls += itemsPerRegion
 	return nil, nil
 }
 
@@ -94,4 +119,41 @@ func buildId(i int, region string) string {
 func regionFromId(id string) string {
 	parts := strings.Split(id, "_")
 	return parts[1]
+}
+
+func getListCalls(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	mut.Lock()
+	defer mut.Unlock()
+	hydrateCalls++
+	return listCalls, nil
+}
+func getHydrateCalls(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	mut.Lock()
+	defer mut.Unlock()
+	hydrateCalls++
+	return hydrateCalls, nil
+}
+
+func doHydrate1(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	return doHydrate("c1", h)
+}
+func doHydrate2(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	return doHydrate("c2", h)
+}
+func doHydrate3(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	return doHydrate("c3", h)
+}
+func doHydrate4(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	return doHydrate("c4", h)
+}
+func doHydrate5(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	return doHydrate("c5", h)
+}
+
+// hydrate call which does not doe anything interesting
+func doHydrate(column string, h *plugin.HydrateData) (interface{}, error) {
+	mut.Lock()
+	defer mut.Unlock()
+	hydrateCalls++
+	return map[string]string{column: fmt.Sprintf("%s_val_%d", column, hydrateCalls)}, nil
 }
