fa3ac5fdd4cc5185cbb21d6a9f1c1e36d54ce9e9
Parth Tyagi <parth.tyagi@turbot.com>
2021-05-27 19:54:30 +0530

    minor updates

--- chaos/table_chaos_key_columns.go
+++ chaos/table_chaos_key_columns.go
@@ -65,18 +65,6 @@ func buildKeyColumns(def *keyColumnTableDefinitions) plugin.HydrateFunc {
 	}
 }
 
-// func buildGetKeyColumns(def *keyColumnTableDefinitions) plugin.HydrateFunc {
-// 	return func(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-// 		i := d.KeyColumnQuals["id"].GetInt64Value()
-// 		columnA := d.KeyColumnQuals["column_a"].GetStringValue()
-// 		columnB := "RANDOM STRING"
-// 		columnC := "RANDOM STRING"
-// 		item := map[string]interface{}{"id": i, "column_a": columnA, "column_b": columnB, "column_c": columnC}
-// 		return item, nil
-
-// 	}
-// }
-
 func calculateKeyColumns(def *keyColumnTableDefinitions) *plugin.KeyColumnSet {
 	if def.call == listCall || def.call == getCall {
 		if def.keyColumnSetType == singleColumn {
@@ -95,19 +83,3 @@ func calculateKeyColumns(def *keyColumnTableDefinitions) *plugin.KeyColumnSet {
 	}
 	return nil
 }
-
-// func calculateGetKeyColumns(def *keyColumnTableDefinitions) *plugin.KeyColumnSet {
-// 	if def.call == getCall {
-// 		if def.keyColumnSetType == singleColumn {
-// 			return plugin.SingleColumn("id")
-// 		}
-// 		if def.keyColumnSetType == anyColumn {
-// 			return plugin.AnyColumn([]string{"id", "column_a"})
-// 		}
-
-// 		if def.keyColumnSetType == allColumns {
-// 			return plugin.AllColumns([]string{"id", "column_a"})
-// 		}
-// 	}
-// 	return plugin.SingleColumn("id")
-// }
