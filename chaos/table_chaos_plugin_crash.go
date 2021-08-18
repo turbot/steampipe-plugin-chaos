70a550339c738643a3f941370a0f53092ad07b19
Puskar Basu <45908484+pskrbasu@users.noreply.github.com>
2021-08-18 18:23:53 +0530

    Add table to simulate plugin crash. Closes #51

---
+++ chaos/table_chaos_plugin_crash.go
@@ -0,0 +1,33 @@
+package chaos
+
+import (
+	"context"
+	"os"
+
+	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/plugin"
+)
+
+func pluginCrashTable() *plugin.Table {
+	return &plugin.Table{
+		Name:        "chaos_plugin_crash",
+		Description: "Chaos table to print 50 rows and do an os.Exit(-1) to simulate a plugin crash",
+		List: &plugin.ListConfig{
+			Hydrate: listIdsFunction,
+		},
+
+		Columns: []*plugin.Column{
+			{Name: "id", Type: proto.ColumnType_INT},
+		},
+	}
+}
+
+func listIdsFunction(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	for i := 0; i < 50; i++ {
+		item := map[string]interface{}{"id": i}
+		d.StreamListItem(ctx, item)
+		os.Exit(-1)
+	}
+
+	return nil, nil
+}
